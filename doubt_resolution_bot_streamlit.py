e8bd7d0fc9bc642c044243b83abb740d2601efe2
siddharthcs187 <siddharthcs187@gmail.com>
2024-09-14 21:52:26 +0530

    Abstraction at course level. Add a config file in JSON format and use all 3 bots.

--- doubt_resolution_bot_streamlit.py
+++ doubt_resolution_bot_streamlit.py
@@ -1,13 +1,14 @@
 import os
 import ast
 import sys
+import json
 from pathlib import Path
 import streamlit as st
 from openai import OpenAI
 from openai import AssistantEventHandler
 from typing_extensions import override
 from openai.types.beta.threads import Text, TextDelta
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
 class EventHandler(AssistantEventHandler):
     """
@@ -45,37 +46,54 @@ class EventHandler(AssistantEventHandler):
         st.session_state.assistant_text.append("")
         st.session_state.chat_history.append(("assistant", text.value))
 
-# load_dotenv()
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# Function to load text template from a file
+def load_text_from_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+   
+# Load JSON file
+def load_json_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if 'lecture_no' not in st.session_state:
     lecture_no = st.query_params["lecture_no"]
     st.session_state.lecture_no = lecture_no
 
-    def load_text_from_file(file_path):
-        with open(file_path, 'r') as file:
-            return file.read()
+course_data = load_json_from_file("config.json")
 
-    template_file = load_text_from_file("prompt_templates/doubt_resolution_prompt_template.txt")
-    topics_file = load_text_from_file('topics/topics_lecture_{0}.txt'.format(
-        st.session_state.lecture_no))
-    system_message_prompt = SystemMessagePromptTemplate.from_template(template_file)
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
-    variables = {
-        "topics": topics_file  
-    }   
-    st.session_state.prompt_file = chat_prompt.format(**variables)
-    #print("\nName of prompt file: ", st.session_state.prompt_file)
-    print(f"\nLength of prompt text: {len(st.session_state.prompt_file)}")
-    print("\n")
+template_file = load_text_from_file("prompt_templates/doubt_resolution_prompt_template.txt")
+
+lecture_no = st.session_state.lecture_no
+course_name = course_data.get("course_name", "Course Name Not Found")
+
+topics_list = course_data.get("lectures", {}).get(lecture_no, {}).get("topics", ["Topics Not Found"])
+topics = "\n".join(topics_list)
+
+variables = {
+    "course_name": course_name,
+    "topics": topics
+}
+
+st.session_state.prompt_file = template_file.format(**variables)
+print(st.session_state.prompt_file)
 
 if 'vector_store_id' not in st.session_state:
     # Create a vector store for book chapter
     vector_store = client.beta.vector_stores.create(
-        name="PPL Book Chapter {0}".format(st.session_state.lecture_no))
+        name="{0} Book Chapter {1}".format(course_name, st.session_state.lecture_no))
 
     # Ready the files for upload to OpenAI
-    file_paths = ["book_chapters/lecture_{0}.pdf".format(st.session_state.lecture_no)]
+    file_paths = ["book_chapters/{0}/lecture_{1}.pdf".format(course_name, st.session_state.lecture_no)]
     file_streams = [open(path, "rb") for path in file_paths]
 
     # Use the upload and poll SDK helper to upload the files, add them to the vector store,
@@ -91,7 +109,7 @@ if 'vector_store_id' not in st.session_state:
 
 teaching_instructions = st.session_state.prompt_file
 assistant = client.beta.assistants.create(
-    name="PPL Doubt Resolver",
+    name="{0} Doubt Resolver".format(course_name),
     instructions=teaching_instructions,
     model="gpt-4o-mini",
     tools=[{"type": "file_search"}],
@@ -101,7 +119,7 @@ assistant = client.beta.assistants.update(
   tool_resources={"file_search": {"vector_store_ids": [st.session_state.vector_store_id]}},
 )
 
-st.title("💬 Doubt Resolution Bot for PPL Lecture {0}".format(st.session_state.lecture_no))
+st.title("💬 Doubt Resolution Bot for {0} Lecture {1}".format(course_name, st.session_state.lecture_no))
 st.write("""
 This bot will resolve your doubts for Lecture {0}. 
 
--- evaluation_bot_streamlit.py
+++ evaluation_bot_streamlit.py
@@ -1,13 +1,14 @@
 import os
 import ast
 import sys
+import json
 from pathlib import Path
 import streamlit as st
 from openai import OpenAI
 from openai import AssistantEventHandler
 from typing_extensions import override
 from openai.types.beta.threads import Text, TextDelta
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
 class EventHandler(AssistantEventHandler):
     """
@@ -45,36 +46,54 @@ class EventHandler(AssistantEventHandler):
         st.session_state.assistant_text.append("")
         st.session_state.chat_history.append(("assistant", text.value))
 
-# load_dotenv()
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# Function to load text template from a file
+def load_text_from_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+   
+# Load JSON file
+def load_json_from_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if 'lecture_no' not in st.session_state:
     lecture_no = st.query_params["lecture_no"]
     st.session_state.lecture_no = lecture_no
 
-    def load_text_from_file(file_path):
-        with open(file_path, 'r') as file:
-            return file.read()
+course_data = load_json_from_file("config.json")
 
-    template_file = load_text_from_file("prompt_templates/evaluation_prompt_template.txt")
-    topics_file = load_text_from_file('topics/topics_lecture_{0}.txt'.format(
-        st.session_state.lecture_no))
-    system_message_prompt = SystemMessagePromptTemplate.from_template(template_file)
-    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
-    variables = {
-        "topics": topics_file  
-    }   
-    st.session_state.prompt_file = chat_prompt.format(**variables)
-    #print("\nName of prompt file: ", st.session_state.prompt_file)
-    print(f"\nLength of prompt text: {len(st.session_state.prompt_file)}")
-    print("\n")
+template_file = load_text_from_file("prompt_templates/evaluation_prompt_template.txt")
+
+lecture_no = st.session_state.lecture_no
+course_name = course_data.get("course_name", "Course Name Not Found")
+
+topics_list = course_data.get("lectures", {}).get(lecture_no, {}).get("topics", ["Topics Not Found"])
+topics = "\n".join(topics_list)
+
+variables = {
+    "course_name": course_name,
+    "topics": topics
+}
+
+st.session_state.prompt_file = template_file.format(**variables)
+print(st.session_state.prompt_file)
 
 if 'vector_store_id' not in st.session_state:
     # Create a vector store for book chapter
-    vector_store = client.beta.vector_stores.create(name="PPL Book Chapter {0}".format(st.session_state.lecture_no))
+    vector_store = client.beta.vector_stores.create(
+        name="{0} Book Chapter {1}".format(course_name, st.session_state.lecture_no))
 
     # Ready the files for upload to OpenAI
-    file_paths = ["book_chapters/lecture_{0}.pdf".format(st.session_state.lecture_no)]
+    file_paths = ["book_chapters/{0}/lecture_{1}.pdf".format(course_name, st.session_state.lecture_no)]
     file_streams = [open(path, "rb") for path in file_paths]
 
     # Use the upload and poll SDK helper to upload the files, add them to the vector store,
@@ -90,10 +109,9 @@ if 'vector_store_id' not in st.session_state:
 
 teaching_instructions = st.session_state.prompt_file
 assistant = client.beta.assistants.create(
-    name="PPL Evaluator",
+    name="{0} Evaluator".format(course_name),
     instructions=teaching_instructions,
     model="gpt-4o-mini",
-    # model="gpt-4o",
     tools=[{"type": "file_search"}],
 )
 assistant = client.beta.assistants.update(
@@ -101,13 +119,12 @@ assistant = client.beta.assistants.update(
   tool_resources={"file_search": {"vector_store_ids": [st.session_state.vector_store_id]}},
 )
 
-st.title("💬 Evaluation Bot for PPL Lecture {0}".format(st.session_state.lecture_no))
-"""
-Please start interaction with the Evaluation Bot by typing in 'Hi', 'Hello' etc. 
-"""
+st.title("💬 Evaluation Bot for {0} Lecture {1}".format(course_name, st.session_state.lecture_no))
+st.write("""
+Please start interaction with the Evaluation Bot by typing in 'Hi', 'Hello' etc.  
+""".format(st.session_state.lecture_no))
 
 text_box = st.empty()
-
 
 # Initialize chat history in session state if not already done
 if 'chat_history' not in st.session_state:
@@ -134,16 +151,6 @@ display_chat_history()
 
 if prompt := st.chat_input("Enter your message"):
     st.session_state.chat_history.append(("user", prompt))
-
-    # Assuming the user input is related to calculating tax
-    # if "tax" in prompt.lower():  # You can adjust this condition based on your actual use case
-    #     try:
-    #         tax_result = calculate_tax(prompt)  # Assuming prompt contains revenue
-    #         st.session_state.chat_history.append(
-    #             ("assistant", f"Tax for revenue {tax_result['revenue']}: {tax_result['tax']}"))
-    #     except ValueError as e:
-    #         st.session_state.chat_history.append(("assistant", str(e)))
-
     message = client.beta.threads.messages.create(
         thread_id=st.session_state.thread_id,
         role="user",
@@ -158,4 +165,4 @@ if prompt := st.chat_input("Enter your message"):
         assistant_id=assistant.id,
         event_handler=EventHandler()
     ) as stream:
-        stream.until_done()
+        stream.until_done() 
